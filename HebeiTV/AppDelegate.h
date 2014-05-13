43781a067d1a29e1f42a3a0d7f002df058a11ff2
Pro <jonas@ProdeMacBook-Pro.local>
2014-05-13 15:04:45 +0800

    Initial Commit

---
+++ HebeiTV/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HebeiTV
+//
+//  Created by Pro on 5/13/14.
+//  Copyright (c) 2014 MyOrganization. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
